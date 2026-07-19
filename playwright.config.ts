929a925e6fdd4031742d0d1e36083bc88a0314ed
Kavitha <49483795+Kavithaqxf2@users.noreply.github.com>
2026-07-19 21:47:16 +0530

    added 4 workers for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0, //outside cicd pipeline//
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html',{ open: 'on-failure' }], ["line"],['allure-playwright']],
   timeout : 60000,
